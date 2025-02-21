216a5ce2a25a5666434e4e300bf67376a976c2be
Victor Bastos <vbastosmaciel@outlook.com.br>
2025-02-21 19:50:56 -0300

    feat: New exception - In case of invalid Id or argument

---
+++ src/main/java/com/project/ggconnect/tcg/exception/InvalidArgumentException.java
@@ -0,0 +1,8 @@
+package com.project.ggconnect.tcg.exception;
+
+public class InvalidArgumentException extends RuntimeException {
+
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+}
